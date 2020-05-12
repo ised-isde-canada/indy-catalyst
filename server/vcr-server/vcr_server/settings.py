a73b4bd1dafc3373f51591e9d2ac34e566c5c465
Mario Bonito <mario.bonito@canada.ca>
2020-05-12 13:55:46 -0400

    wip: goc theme work
    Signed-off-by: Mario Bonito <mario.bonito@canada.ca>

--- server/vcr-server/vcr_server/settings.py
+++ server/vcr-server/vcr_server/settings.py
@@ -143,9 +143,7 @@ if CONN_MAX_AGE < 0:
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
-    },
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
@@ -258,16 +256,16 @@ INDY_HOLDER_ID = "TheOrgBook_Holder"
 APPLICATION_URL = os.getenv("APPLICATION_URL") or "http://localhost:8080"
 
 API_METADATA = {
-    "title": "OrgBook BC API",
-    "description": "OrgBook BC is a public, searchable directory of digital records for registered "
-    "businesses in the Province of British Columbia. Over time, other government "
-    "organizations and businesses will also begin to issue digital records through "
-    "OrgBook BC. For example, permits and licenses issued by various government services.",
-    "terms": {"url": "https://www2.gov.bc.ca/gov/content/data/open-data"},
-    "contact": {"email": "bcdevexchange@gov.bc.ca"},
+    "title": "OrgBook Canada API",
+    "description": "OrgBook Canada is a public, searchable directory of digital records for federally incorporated "
+    "businesses in the Canada. Over time, other government "
+    "organizations and businesses will also begin to issue digital records to "
+    "OrgBook Canada. For example, permits and licenses issued by various government services.",
+    "terms": {"url": "https://open.canada.ca/en/open-data"},
+    "contact": {"email": "open-ouvert@tbs-sct.gc.ca"},
     "license": {
-        "name": "Open Government License - British Columbia",
-        "url": "https://www2.gov.bc.ca/gov/content/data/open-data/api-terms-of-use-for-ogl-information",
+        "name": "Open Government License - Canada",
+        "url": "https://open.canada.ca/en/open-government-licence-canada",
     },
 }
 
